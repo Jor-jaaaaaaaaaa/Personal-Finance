ccedc5e326978312bac89f22fb1534ee560e444e
Krit Vasupen <kritjaja06@gmail.com>
2026-01-14 00:58:25 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   node_modules/.package-lock.json
    	modified:   node_modules/.vite/deps/_metadata.json
    	renamed:    node_modules/.vite/deps/chunk-HHEDW4YO.js -> node_modules/.vite/deps/chunk-5YKXUS5S.js
    	renamed:    node_modules/.vite/deps/chunk-HHEDW4YO.js.map -> node_modules/.vite/deps/chunk-5YKXUS5S.js.map
    	deleted:    node_modules/.vite/deps/chunk-VUNV25KB.js
    	deleted:    node_modules/.vite/deps/chunk-VUNV25KB.js.map
    	deleted:    node_modules/.vite/deps/express.js
    	deleted:    node_modules/.vite/deps/express.js.map
    	modified:   node_modules/.vite/deps/react-dom_client.js
    	modified:   node_modules/.vite/deps/react-dom_client.js.map
    	modified:   node_modules/.vite/deps/react.js
    	new file:   node_modules/cors/CONTRIBUTING.md
    	new file:   node_modules/cors/HISTORY.md
    	new file:   node_modules/cors/LICENSE
    	new file:   node_modules/cors/README.md
    	new file:   node_modules/cors/lib/index.js
    	new file:   node_modules/cors/package.json
    	new file:   node_modules/object-assign/index.js
    	new file:   node_modules/object-assign/license
    	new file:   node_modules/object-assign/package.json
    	new file:   node_modules/object-assign/readme.md
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/Backend_App.js

--- src/Backend_App.js
+++ src/Backend_App.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+}
 
-app.get("fif",(req,res)=>{
-    res.send("something")
+app.use(cors(corsOptions))
+
+app.get("/",(req,res)=>{
+    res.send(console.log("Is it working"))
 })
 
-app.listen(5173,()=>{
-    console.log("hello")
+app.listen(8080,()=>{
+    console.log("Server running")
 })
